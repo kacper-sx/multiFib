3d238f047b372d1123aed7087bdae78e897a7783
unknown <kacper.saweczko@o2.pl>
2023-01-31 15:07:32 +0100

    repo init

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      multifib Kacper Saweczko gr1.4
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
